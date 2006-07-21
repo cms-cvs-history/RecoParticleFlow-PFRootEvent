1128b15adfb21c586922c2ab41a949abcca75d4d
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-07-21 16:24:01 +0000

    first commit

---
+++ test/test.C
@@ -0,0 +1,12 @@
+{
+
+gSystem->Load("libFWCoreFWLite.so");
+gSystem->Load("libRecoParticleFlowPFRootEvent.so");
+AutoLibraryLoader::enable();
+gSystem->Load("libCintex.so");
+ROOT::Cintex::Cintex::Enable();
+
+PFRootEventManager em("pfRootEvent.opt");
+int i=0;
+em.Display(i++);
+}
