18baea8fa22482f4c6d64b6d0ee4d77f2f846a3a
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-09-21 16:45:15 +0000

    new comments to opt file, particle type displaid

--- test/test.C
+++ test/test.C
@@ -1,13 +1,19 @@
 {
-
+// initialize the core of the framework, and load the PFRootEvent 
+// library, which contains the ROOT interface
 gSystem->Load("libFWCoreFWLite.so");
 gSystem->Load("libRecoParticleFlowPFRootEvent.so");
 AutoLibraryLoader::enable();
 gSystem->Load("libCintex.so");
 ROOT::Cintex::Cintex::Enable();
 
+// create a PFRootEventManager
 PFRootEventManager em("pfRootEvent.opt");
-int i=0;
+
+// display entry number 2
+int i=2;
 em.display(i++);
-// em.lookForMaxRecHit(true);
+
+// look for ECAL rechit with maximum energy
+em.lookForMaxRecHit(true);
 }
