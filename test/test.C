f252162db3d58a5a8be45ce5ad4742e7dc073200
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-10-23 14:28:52 +0000

    prod branch should be ok now

--- test/test.C
+++ test/test.C
@@ -11,7 +11,7 @@ ROOT::Cintex::Cintex::Enable();
 PFRootEventManager em("pfRootEvent.opt");
 
 // display entry number 2
-int i=2;
+int i=0;
 em.display(i++);
 
 // look for ECAL rechit with maximum energy
