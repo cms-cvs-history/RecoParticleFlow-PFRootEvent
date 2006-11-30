aa3f29ef0497f9073f99ee42786dac35aa85cdcd
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2006-11-30 08:46:32 +0000

    changed the name: EffResImp->ResidualFitter

--- src/classes.h
+++ src/classes.h
@@ -1,7 +1,7 @@
 #include "RecoParticleFlow/PFRootEvent/interface/PFRootEventManager.h"
 #include "RecoParticleFlow/PFRootEvent/interface/PFRootEventManagerColin.h"
 #include "RecoParticleFlow/PFRootEvent/interface/EventColin.h"
-#include "RecoParticleFlow/PFRootEvent/interface/EffResImp.h"
+#include "RecoParticleFlow/PFRootEvent/interface/ResidualFitter.h"
 
 namespace { 
   namespace {
