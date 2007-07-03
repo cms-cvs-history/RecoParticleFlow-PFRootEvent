97db1c96eba428b4f42f03fbda57de4927d658dd
Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch>
2007-07-03 08:38:25 +0000

    added a few macros

---
+++ test/Macros/tauBenchmarkDisplay_famos.C
@@ -0,0 +1,20 @@
+{
+// initialize the core of the framework, and load the PFRootEvent 
+// library, which contains the ROOT interface
+gSystem->Load("libFWCoreFWLite.so");
+gSystem->Load("libRecoParticleFlowPFRootEvent.so");
+// AutoLibraryLoader::enable();
+gSystem->Load("libCintex.so");
+ROOT::Cintex::Cintex::Enable();
+
+// create a PFRootEventManager
+// PFRootEventManager em("pfRootEvent.opt");
+PFRootEventManager em("tauBenchmark_famos.opt");
+
+// display entry number 2
+int i=0;
+em.display(i++);
+
+// look for ECAL rechit with maximum energy
+em.lookForMaxRecHit(true);
+}
