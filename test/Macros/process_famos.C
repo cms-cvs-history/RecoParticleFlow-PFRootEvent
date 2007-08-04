b0a5840a216d2ef1e0f020403ea4868bf951382c
cmsbuild <>
2007-08-04 18:18:02 +0000

    This commit was manufactured by cvs2git to create tag 'RecoParticleFlow- PFRootEvent-CMSSW_1_7_0_pre4'.
    Sprout from master 2007-08-04 18:17:58 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'had forgotten the data/ and test/ directories. Corrected. Head should now be compatible with 1_7_X'
    Cherrypick from RecoParticleFlow-PFRootEvent-compat_1_5_0 2007-07-10 16:23:17 UTC Colin Bernet <sha1-d13205ce5ee454336999e735bc1914cf98d16be2@cern.ch> 'all objects are now clickable. first version of a GUI (merged from the 1_3_1 head)':
        test/Macros/Gui.C
        test/Macros/compareTauPlot.C
        test/Macros/display.C
        test/Macros/display_famos.C
        test/Macros/eCalib.C
        test/Macros/init.C
        test/Macros/makeTauPlot.C
        test/Macros/makeTauPlotTree.C
        test/Macros/process.C
        test/Macros/process_famos.C
        test/Macros/tauBenchmarkDisplay.C
        test/Macros/tauBenchmarkDisplay_famos.C
        test/Macros/tauBenchmarkProcess.C
        test/Macros/tauBenchmarkProcess_famos.C

--- test/Macros/process_famos.C
+++ test/Macros/process_famos.C
@@ -1,19 +1,23 @@
 {
 // initialize the core of the framework, and load the PFRootEvent 
 // library, which contains the ROOT interface
+
 gSystem->Load("libFWCoreFWLite.so");
 gSystem->Load("libRecoParticleFlowPFRootEvent.so");
-// // AutoLibraryLoader::enable();
+// AutoLibraryLoader::enable();
 gSystem->Load("libCintex.so");
 ROOT::Cintex::Cintex::Enable();
 
 // create a PFRootEventManager
-PFRootEventManager em("pfRootEvent.opt");
+PFRootEventManager em("pfRootEvent_famos.opt");
 
-// display first entry
-int i=0;
-em.display(i++);
-
-// look for ECAL rechit with maximum energy
-em.lookForMaxRecHit(true);
+if(em.tree() ) {
+  int n =  em.tree()->GetEntries();
+  for(unsigned i=0; i<n; i++) {
+    em.processEntry(i);
+    // em.print();
+  }
+  em.write();
+}
+gApplication->Terminate(); 
 }
